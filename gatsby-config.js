00c8b343e0854e600145f318f6e4f968ef97ef0a
sharansajivmenon <sharansajivmenon@gmail.com>
2022-11-08 10:38:33 -0500

    changed site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Contentful Starter",
-    description: "Official Contentful Gatsby Starter",
+    title: "Sharan Sajiv Menon",
+    description: "My Personal Website",
   },
   plugins: [
     "gatsby-transformer-sharp",
